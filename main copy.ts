deb8bda562c219972122f7f0765e6b6cb8730b1f
jakobhaulund <jakobhaulund@hotmail.com>
2022-11-23 08:48:47 +0100

    shuaashdhja

--- main copy.ts
+++ main copy.ts
@@ -2,6 +2,15 @@ const { Client } = require("pg");
 const express = require("express");
 const morgan = require("morgan"); // Some nice logging
 
+/**
+ * Følgende parametre bruges til at forbinde til databasen.
+ * PORT er den port som webserveren her kører på.
+ * DB_USER er brugernavnet til databasen.
+ * DB_HOST er serveren som databasen kører på. Enten localhost eller en anden server.
+ * DB_NAME er det navn som databasen har.
+ * DB_PW er password til DB_USER.
+ * DB_PORT er porten til databasen. Det plejer at være 5432, så den behøver man nok ikke ændre.
+ */
 const PORT = process.env.PORT || 8080;
 const DB_USER = process.env.DB_USER || "lzdkbzkl";
 const DB_HOST = process.env.DB_HOST || "mouse.db.elephantsql.com";
@@ -9,6 +18,27 @@ const DB_NAME = process.env.DB_NAME || "lzdkbzkl";
 const DB_PW = process.env.DB_PW || "VYWTjVbx3KdcdaNK2H9ExTPxTzmHvlhH";
 const DB_PORT = process.env.DB_PORT || 5432;
 
+/**
+ * I stedet for at ændre på DB-værdierne i koden herover, er det bedre at gøre det som
+ * en del af den måde man kører programmet på. Hver DB-værdi kan sættes i terminalen
+ * inden man kører programmet. Det gør man sådan her:
+ *
+ * $ export DB_NAME="kristians-database"
+ *
+ * Nu er DB_NAME sat til "kristians-database" når programmet kører, uden at man har
+ * ændret i JavaScript-koden. Dette skal gøres hver gang du åbner en ny terminal.
+ * Det skal helst gøres både for DB_NAME, DB_PW, DB_USER og DB_HOST.
+ * PORT og DB_PORT plejer man ikke at ændre.
+if (!process.env.DB_NAME || !process.env.DB_PW || !process.env.DB_USER) {
+  console.warn("Husk at sætte databasenavn, password og user via env vars.");
+  console.warn("Eksempel på at sætte databasenavn i terminalen:");
+  console.warn(`export DB_NAME="kristians-database"`);
+  console.warn("Lige nu er databasenavn sat til:", DB_NAME);
+} else {
+  console.log("Postgres database:", DB_NAME);
+  console.log("Postgres user:", DB_USER);
+}
+*/
 /*
  * Herunder laves web-serveren. Den indeholder din html (fra public-folderen)
  * og API'en så der er forbindelse videre til databasen fra JavaScript. Det er "to i en".
@@ -21,7 +51,6 @@ const client = new Client({
   password: DB_PW,
   port: DB_PORT,
 });
-
 client.connect();
 
 app.use(express.text());
@@ -44,7 +73,7 @@ app.use(morgan("combined"));
 
 //Her defineres API'en.
 
-app.get("api/years", async (req, res) => {
+app.get("/api/years", async (req, res) => {
   try {
     const query1 = `SELECT years FROM climate`;
     queryData = await client.query(query1);
